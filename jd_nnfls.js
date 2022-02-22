5580d9d082397e7f14e219b32660fa8febe7135c
a00126 <87116510+a00126@users.noreply.github.com>
2022-02-22 11:01:16 +0800

    Update jd_nnfls.js

--- jd_nnfls.js
+++ jd_nnfls.js
@@ -54,7 +54,7 @@ if ($.isNode()) {
     }
     shareCodes = shareCodes.filter(code => code)
     const author = Math.random() > 0.5 ? 'zero205' : 'ZXX2021'
-    await getShareCode('nnfls.json', author, 3, true)
+    await getShareCode('', author, 3, true)
     shareCodes = [...new Set([...shareCodes, ...($.shareCode || [])])];
     if (shareCodes.length > 0) {
         console.log(`\n*********开始互助**********\n`);
